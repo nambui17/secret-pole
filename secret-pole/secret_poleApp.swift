61ec8fb1563d32bcb7a9991bedc8266ae49d71e3
nambui <nam.bui17@icloud.com>
2023-08-25 09:35:07 -0500

    Initial commit

---
+++ secret-pole/secret_poleApp.swift
@@ -0,0 +1,17 @@
+//
+//  secret_poleApp.swift
+//  secret-pole
+//
+//  Created by Nam Bui on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct secret_poleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
